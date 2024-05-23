4b1602aec7a4359d7ab8085c83fe33f195dd8740
elieljohn <ejmalabar88@gmail.com>
2024-05-23 14:12:32 +0100

    Implementing MatTables
    1. Copy 'displayedColumns' and 'dataSource' properties from the Tables component of https://material.angular.io/components/table/overview#datasource then modify accordingly
    notes.component.ts: line 14-16
    
    2.  Modify ngOnInit to initialize dataSource
    notes.component.ts: line 21-23
    
    3. Copy .scss script from https://material.angular.io/components/table/overview#datasource
    notes.component.scss
    
    4. Copy and modify markup from
    https://material.angular.io/components/table/overview#datasource
    notes.component.html

--- angularmaterial/src/app/contactmanager/components/notes/notes.component.ts
+++ angularmaterial/src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
 import { Note } from '../../models/note';
 
 @Component({
@@ -10,9 +12,15 @@ export class NotesComponent implements OnInit {
 
   @Input() notes: Note[] = [];  // Initialize notes as an empty array to avoid error
 
+  displayedColumns: string[] = ['position', 'title', 'date'];
+  dataSource!: MatTableDataSource<Note>;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    // Create new instance and pass notes array to dataSource
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
 }
